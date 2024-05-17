4ed25220b9407df48b6c74697392abb09ec61aba
Long Duong Nhat <128609047+Odoo-Tangerine@users.noreply.github.com>
2024-05-17 18:05:36 +0700

    Hot fix post init hook

--- tangerine_bank_sync/__init__.py
+++ tangerine_bank_sync/__init__.py
@@ -1,5 +1,7 @@
 from . import models
+from odoo import api, SUPERUSER_ID
 
 
-def bank_information_sync(env):
+def bank_information_sync(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
     env['res.bank'].bank_information_sync()
